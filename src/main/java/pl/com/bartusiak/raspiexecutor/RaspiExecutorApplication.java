f440ad798f4f1c2701e503b79968730bb7f4f4f5
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-02-08 19:04:16 +0100

    Initial commit

---
+++ src/main/java/pl/com/bartusiak/raspiexecutor/RaspiExecutorApplication.java
@@ -0,0 +1,13 @@
+package pl.com.bartusiak.raspiexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspiExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspiExecutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/bartusiak/raspiexecutor/RaspiExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.bartusiak.raspiexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspiExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
