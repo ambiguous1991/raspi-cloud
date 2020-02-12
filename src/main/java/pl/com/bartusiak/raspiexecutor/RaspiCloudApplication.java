25edce2e1789fccc87a3d4aedc3f5fb5fbe51a4a
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-02-12 22:29:55 +0100

    Eureka

--- src/main/java/pl/com/bartusiak/raspiexecutor/RaspiCloudApplication.java
+++ src/main/java/pl/com/bartusiak/raspiexecutor/RaspiCloudApplication.java
@@ -3,13 +3,15 @@ package pl.com.bartusiak.raspiexecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @EnableConfigServer
 @SpringBootApplication
-public class RaspiExecutorApplication {
+public class RaspiCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RaspiExecutorApplication.class, args);
+		SpringApplication.run(RaspiCloudApplication.class, args);
 	}
 
 }
--- src/test/java/pl/com/bartusiak/raspiexecutor/RaspiCloudApplicationTests.java
+++ src/test/java/pl/com/bartusiak/raspiexecutor/RaspiCloudApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RaspiExecutorApplicationTests {
+class RaspiCloudApplicationTests {
 
 	@Test
 	void contextLoads() {
