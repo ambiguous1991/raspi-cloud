5c7dab951bb69555aab1e7fbab7b28ccb6323170
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-04-12 14:54:09 +0200

    CORS

--- src/main/java/pl/com/bartusiak/raspiexecutor/RaspiCloudApplication.java
+++ src/main/java/pl/com/bartusiak/raspiexecutor/RaspiCloudApplication.java
@@ -4,16 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableEurekaServer
 @EnableConfigServer
 @EnableScheduling
+@Configuration
 @SpringBootApplication
-public class RaspiCloudApplication {
+public class RaspiCloudApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaspiCloudApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 }
